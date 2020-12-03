95ac3c077a1394c02870acf97a3c65fa9144b9b8
Marc Gurevitx <marcgurevitx@gmail.com>
2020-12-03 19:48:59 +0300

    Tests for request

--- test_goodenough.py
+++ test_goodenough.py
@@ -51,7 +51,7 @@ def test_rules_should_be_coro_funcs():
 def test_clamped_coefs():
 
     async def rule_greatest_the_wrong(request, item):
-        return item  # will get clamped inside 0..1, so in this case will always be 1.
+        return item  # will get clamped inside 0..1, so in case of [1,2,3,4,5] will always be 1.
 
     async def get_sample(request):
         return [1, 2, 3, 4, 5]
@@ -60,13 +60,40 @@ def test_clamped_coefs():
     assert ge.pick({}) == 1  # and not 5
 
 
-def test_xxxxxxxxx():
+def test_request_in_rules():
+
+    async def rule_close_to_request(request, item):  # request can be anything, it's a number in this example
+        return 1 - abs(request - item)
+
+    async def get_sample(request):
+        return [1, 2, 3, 4, 5]
+
+    ge = GoodEnough(get_sample, rules=[rule_close_to_request])
+    assert ge.pick(request=2.8) == 3  # item 3 is the closest to 2.8
+
+
+def test_request_in_get_sample():
+
+    async def get_sample(request):
+        return range(request["from"], request["to"])  # request can be anything, it's a dict in this example
+
+    ge = GoodEnough(get_sample, rules=[])
+    assert ge.pick(request={"from": 7, "to": 11}) == 7  # no rules -- return the first
+
+
+def test_xxxxxxxxxxx():
+
+
+
+
+
+
 
     55/0
 
 
 
-# request
 # updater
 # never return items w 0 coef (or let define strategy: retry n times, raise, still return)
 # rules weights
+# silent clamp? shouldn't we complain / throw warning?
