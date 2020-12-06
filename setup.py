23151770cac486d2ffe785011e814fae651d87d7
Marc Gurevitx <marcgurevitx@gmail.com>
2020-12-06 18:56:48 +0300

    Small change in description

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setup(
     name="goodenough",
     version="0.0.2",
-    description="Lib to pick the \"best\" item from a random sample",
+    description="Lib to pick the \"best\" item from a collection or storage",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/marcgurevitx/goodenough",
